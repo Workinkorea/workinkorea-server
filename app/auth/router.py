b1ad47c3829f3be49769da99a4c0303cf251c2cb
김병민 <bmkim766@gmail.com>
2025-09-20 18:11:33 +0900

    feat: Add email verification endpoint.

--- app/auth/router.py
+++ app/auth/router.py
@@ -1,8 +1,11 @@
 # app/auth/router.py
 from fastapi import APIRouter
+from starlette.responses import JSONResponse
+from app.auth.models import EmailSchema
+from app.auth.service import send_email_verifi_code
 
 router = APIRouter(
-    prefix="/auth",
+    prefix="/api/auth",
     tags=["auth"]
     # dependencies=[Depends(get_token_header)],
     # responses={404: {"description": "Not found"}}
@@ -15,3 +18,17 @@ def login():
 @router.post("/logout")
 def logout():
     return {"message": "logout"}
+
+
+@router.post("/email")
+async def simple_send(email: EmailSchema) -> JSONResponse:
+    """
+    email verification code send
+    args:
+        email: EmailSchema
+    """
+    try:
+        await send_email_verifi_code(email)
+    except Exception as e:
+        return JSONResponse(status_code=500, content={"message": str(e)})
+    return JSONResponse(status_code=200, content={"message": "email has been sent"})     
