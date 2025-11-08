333bbca9af073d330428f7c128a216bf98175328
김병민 <bmkim766@gmail.com>
2025-11-08 15:12:34 -0700

    feat: Add get_current_company to CompanyService

--- app/auth/services/company.py
+++ app/auth/services/company.py
@@ -1,8 +1,7 @@
 from sqlalchemy.ext.asyncio import AsyncSession
 from app.auth.repositories.company import CompanyRepository
 from app.auth.models import Company, CompanyUser
-from fastapi import HTTPException
-from fastapi.responses import JSONResponse
+from fastapi import HTTPException, Request
 import hashlib
 
 from app.core.settings import SETTINGS
@@ -36,11 +35,11 @@ class CompanyService:
         company_user = await self.company_repository.get_company_user_by_email(company_user_data['email'])
 
         if not company_user:
-            return None
+            raise ValueError("company user not found")
 
         password_hash = hashlib.sha256(company_user_data['password'].encode('utf-8')).hexdigest()
         if password_hash != company_user.password:
-            return None
+            raise ValueError("password is incorrect")
         
         return company_user
 
@@ -92,3 +91,21 @@ class CompanyService:
             return refresh_token
         except Exception as e:
             raise e
+
+    async def get_current_company(self, request:Request) -> Company | None:
+        """
+        get current company
+        args:
+            request: Request
+        """
+        access_token = request.cookies.get("access_token")
+
+        if not access_token:
+            raise ValueError("Not authenticated")
+        
+        payload = jwt.decode(access_token, SETTINGS.JWT_SECRET, algorithms=[SETTINGS.JWT_ALGORITHM])
+        company_id = payload.get("company_id")
+        if not company_id:
+            raise ValueError("Company ID not found")
+
+        return await self.company_repository.get_company_by_company_id(int(company_id))
