3ddd5b8793d53b7f7cda9a0984b36d57ff4ed6b2
김병민 <bmkim766@gmail.com>
2025-11-08 13:06:31 -0700

    feat: Implement JWT token creation for company access and refresh in CompanyService

--- app/auth/services/company.py
+++ app/auth/services/company.py
@@ -1,8 +1,13 @@
 from sqlalchemy.ext.asyncio import AsyncSession
 from app.auth.repositories.company import CompanyRepository
 from app.auth.models import Company, CompanyUser
+from fastapi import HTTPException
 from fastapi.responses import JSONResponse
 import hashlib
+
+from app.core.settings import SETTINGS
+import datetime
+import jwt
 
 
 class CompanyService:
@@ -29,12 +34,13 @@ class CompanyService:
         get company user by email
         """
         company_user = await self.company_repository.get_company_user_by_email(company_user_data['email'])
+
         if not company_user:
-            return JSONResponse(content={"error": "Company user not found"}, status_code=404)
+            return None
 
         password_hash = hashlib.sha256(company_user_data['password'].encode('utf-8')).hexdigest()
         if password_hash != company_user.password:
-            return JSONResponse(content={"error": "Invalid password"}, status_code=400)
+            return None
         
         return company_user
 
@@ -48,3 +54,41 @@ class CompanyService:
         # password hash
         company_user_data['password'] = hashlib.sha256(company_user_data['password'].encode('utf-8')).hexdigest()
         return await self.company_repository.create_company_user_to_db(company_user_data)
+
+    async def create_access_company_token(self, user_email: str, company_id: int) -> str:
+        """
+        create access company token
+        args:
+            user_email: str
+        """
+        try:
+            to_encode = {
+                "sub": user_email,
+                "company_id": company_id,
+                "exp": datetime.datetime.utcnow() + datetime.timedelta(minutes=SETTINGS.ACCESS_TOKEN_EXPIRE_MINUTES),
+                "type": "access_company"
+            }
+            access_token = jwt.encode(
+                to_encode, SETTINGS.JWT_SECRET, algorithm=SETTINGS.JWT_ALGORITHM)
+            return access_token
+        except Exception as e:
+            raise e
+
+    async def create_refresh_company_token(self, user_email: str, company_id: int) -> str:
+        """
+        create refresh company token
+        args:
+            user_email: str
+        """
+        try:
+            to_encode = {
+                "sub": user_email,
+                "company_id": company_id,
+                "exp": datetime.datetime.utcnow() + datetime.timedelta(minutes=SETTINGS.REFRESH_TOKEN_EXPIRE_MINUTES),
+                "type": "refresh_company"
+            }
+            refresh_token = jwt.encode(
+                to_encode, SETTINGS.JWT_SECRET, algorithm=SETTINGS.JWT_ALGORITHM)
+            return refresh_token
+        except Exception as e:
+            raise e
