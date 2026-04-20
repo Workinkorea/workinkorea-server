23e831d70251fc2c7f5e0a52f36f150bbfc01501
김병민 <bmkim766@gmail.com>
2026-04-19 22:28:29 -0600

    Fix : Update Api router issue

--- app/applications/router.py
+++ app/applications/router.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter
 from app.applications.routers.application import router as application_router
 
-router = APIRouter(prefix="/api/applications", tags=["applications"])
-router.include_router(application_router)
+router = APIRouter()
+router.include_router(application_router, prefix="/api/applications", tags=["applications"])
